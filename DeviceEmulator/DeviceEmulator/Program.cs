84a3d7810c90e51e027fecf82bde5b01c76834bd
ynesterenko <yurii.nesterenko@gmail.com>
2018-12-15 23:10:10 +0200

    DeviceEmulator project

---
+++ DeviceEmulator/DeviceEmulator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DeviceEmulator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const String noStationId = "==not defined==";
+			String[] parts = new string[] { "help" };
+			String stationId = noStationId;
+			String markId = String.Empty;
+
+			do
+			{
+				switch (parts[0])
+				{
+					case "quit":
+						return;
+
+					case "help":
+						Console.WriteLine("Allowed commands:");
+						Console.WriteLine(" help");
+						Console.WriteLine(" quit");
+						Console.WriteLine(" setstationid #ID");
+						Console.WriteLine(" foundmark #ID");
+						Console.WriteLine(" lostmark #ID");
+						break;
+
+					case "setstationid":
+						if (parts[1].Length > 0)
+						{
+							stationId = parts[1];
+							Console.WriteLine("Station ID is set to " + stationId);
+						}
+						else
+						{
+							Console.WriteLine("Error: no station ID defined");
+						}
+						break;
+
+					case "foundmark":
+						if (stationId == noStationId)
+						{
+							Console.WriteLine("Station ID is not set yet");
+						}
+						else if (parts[1].Length > 0)
+						{
+							markId = parts[1];
+							RequestServer("POST", stationId, markId);
+							Console.WriteLine("Found mark #" + markId);
+						}
+						else
+						{
+							Console.WriteLine("Error: no mark ID defined");
+						}
+						break;
+
+					case "lostmark":
+						if (stationId == noStationId)
+						{
+							Console.WriteLine("Station ID is not set yet");
+						}
+						else if (parts[1].Length > 0)
+						{
+							markId = parts[1];
+							RequestServer("DELETE", stationId, markId);
+							Console.WriteLine("Lost mark #" + markId);
+						}
+						else
+						{
+							Console.WriteLine("Error: no mark ID defined");
+						}
+						break;
+
+					default:
+						Console.WriteLine("Unknown Command: " + parts[0]);
+						break;
+				}
+
+				Console.Write("Next command? ");
+				parts = Console.ReadLine().Split(' ');
+				if(parts.Length < 2)
+				{
+					Array.Resize(ref parts, 2);
+					parts[1] = String.Empty;
+				}
+
+			} while (true);
+		}
+
+		private static void RequestServer(string operation, string stationId, string markId)
+		{
+			WebRequest request = WebRequest.Create(ConfigurationManager.AppSettings[operation == "DELETE" ? "lostmarkUrl" : "foundmarkUrl"]);
+			request.Method = operation;
+			byte[] dataStream = Encoding.UTF8.GetBytes(String.Format("station={0}&mark={1}", stationId, markId));
+			Stream newStream = request.GetRequestStream();
+			newStream.Write(dataStream, 0, dataStream.Length);
+			newStream.Close();
+
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		}
+	}
+}
---
+++ DeviceEmulator/DeviceEmulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DeviceEmulator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeviceEmulator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("619f48b7-e035-4766-902d-ab36d45c5285")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
