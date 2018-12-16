f46ef6154222268aab91a39854d6e7203de4e4d2
scorpion1011 <vladislav.nesterenko@gmail.com>
2018-12-16 12:55:24 +0200

    friends.. backend creating

---
+++ DynamicEquipmentSystem/ViewModels/AddFriendViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel.DataAnnotations;
+
+namespace DynamicEquipmentSystem.ViewModels
+{
+    public class AddFriendViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
